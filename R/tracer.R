cc8fcf080ca28026dd04fd641d7d35c0bd9e1f18
Aviral Goel <goel.aviral@gmail.com>
2021-03-06 14:07:43 -0500

    Add function to trace code from file and move writing logic to experimentr

--- R/tracer.R
+++ R/tracer.R
@@ -1,9 +1,9 @@
 
 #' @export
 #' @importFrom instrumentr trace_code get_exec_stats
-profile_laziness <- function(code,
-                             environment = parent.frame(),
-                             quote = TRUE) {
+profile_expr <- function(code,
+                         environment = parent.frame(),
+                         quote = TRUE) {
     tracer <- .Call(C_lazr_tracer_create)
 
     if(quote) {
@@ -16,5 +16,16 @@ profile_laziness <- function(code,
 
     state$exec_stats <- get_exec_stats(tracer)
 
-    state
+    list(data = list(calls = state$calls,
+                     arguments = state$arguments,
+                     exec_stats = state$exec_stats))
+}
+
+#' @export
+profile_file <- function(file, environment = parent.frame()) {
+    code <- parse(file = file)
+
+    code <- as.call(c(`{`, code))
+
+    profile_expr(code, quote = FALSE)
 }
