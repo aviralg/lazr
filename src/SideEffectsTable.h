bc1ee59a77cbd6342a46d18304d4bab5e5563578
Aviral Goel <goel.aviral@gmail.com>
2021-03-26 10:11:42 -0400

    Fix environment column names and variables and remove redundant ones

--- src/SideEffectsTable.h
+++ src/SideEffectsTable.h
@@ -14,83 +14,56 @@ class SideEffectsTable {
     }
 
     void insert(const std::string& type,
-                const std::string& varname,
+                const std::string& var_name,
                 bool transitive,
                 instrumentr_promise_t promise,
                 Environment* environment) {
-        int promise_id = instrumentr_promise_get_id(promise);
+        int arg_id = instrumentr_promise_get_id(promise);
 
         type_.push_back(type);
-        varname_.push_back(varname);
+        var_name_.push_back(var_name);
         transitive_.push_back(transitive);
-        promise_id_.push_back(promise_id);
-        environment_id_.push_back(environment->get_id());
-        environment_type_.push_back(environment->get_type());
-        environment_name_.push_back(environment->get_name());
-        environment_call_id_.push_back(environment->get_call_id());
+        arg_id_.push_back(arg_id);
+        env_id_.push_back(environment->get_id());
     }
 
     SEXP to_sexp() {
         int size = type_.size();
 
         SEXP r_type = PROTECT(allocVector(STRSXP, size));
-        SEXP r_varname = PROTECT(allocVector(STRSXP, size));
+        SEXP r_var_name = PROTECT(allocVector(STRSXP, size));
         SEXP r_transitive = PROTECT(allocVector(LGLSXP, size));
-        SEXP r_promise_id = PROTECT(allocVector(INTSXP, size));
-        SEXP r_environment_id = PROTECT(allocVector(INTSXP, size));
-        SEXP r_environment_type = PROTECT(allocVector(STRSXP, size));
-        SEXP r_environment_name = PROTECT(allocVector(STRSXP, size));
-        SEXP r_environment_call_id = PROTECT(allocVector(INTSXP, size));
+        SEXP r_arg_id = PROTECT(allocVector(INTSXP, size));
+        SEXP r_env_id = PROTECT(allocVector(INTSXP, size));
 
         int index = 0;
         for (int index = 0; index < size; ++index) {
             SET_STRING_ELT(r_type, index, make_char(type_[index]));
-            SET_STRING_ELT(r_varname, index, make_char(varname_[index]));
+            SET_STRING_ELT(r_var_name, index, make_char(var_name_[index]));
             SET_LOGICAL_ELT(r_transitive, index, transitive_[index]);
-            SET_INTEGER_ELT(r_promise_id, index, promise_id_[index]);
-            SET_INTEGER_ELT(r_environment_id, index, environment_id_[index]);
-            SET_STRING_ELT(
-                r_environment_type, index, make_char(environment_type_[index]));
-            SET_STRING_ELT(
-                r_environment_name, index, make_char(environment_name_[index]));
-            SET_INTEGER_ELT(
-                r_environment_call_id, index, environment_call_id_[index]);
+            SET_INTEGER_ELT(r_arg_id, index, arg_id_[index]);
+            SET_INTEGER_ELT(r_env_id, index, env_id_[index]);
         }
 
-        std::vector<SEXP> columns({r_type,
-                                   r_varname,
-                                   r_transitive,
-                                   r_promise_id,
-                                   r_environment_id,
-                                   r_environment_type,
-                                   r_environment_name,
-                                   r_environment_call_id});
+        std::vector<SEXP> columns(
+            {r_type, r_var_name, r_transitive, r_arg_id, r_env_id});
 
-        std::vector<std::string> names({"type",
-                                        "varname",
-                                        "transitive",
-                                        "promise_id",
-                                        "environment_id",
-                                        "environment_type",
-                                        "environment_name",
-                                        "environment_call_id"});
+        std::vector<std::string> names(
+            {"type", "var_name", "transitive", "arg_id", "env_id"});
 
         SEXP df = create_data_frame(names, columns);
 
-        UNPROTECT(8);
+        UNPROTECT(5);
 
         return df;
     }
 
   private:
     std::vector<std::string> type_;
-    std::vector<std::string> varname_;
+    std::vector<std::string> var_name_;
     std::vector<bool> transitive_;
-    std::vector<int> promise_id_;
-    std::vector<int> environment_id_;
-    std::vector<std::string> environment_type_;
-    std::vector<std::string> environment_name_;
-    std::vector<int> environment_call_id_;
+    std::vector<int> arg_id_;
+    std::vector<int> env_id_;
 };
 
 #endif /* LAZR_SIDE_EFFECTS_TABLE_H */
